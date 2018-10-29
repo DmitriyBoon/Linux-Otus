0088cbf15b98f8575a844bf6caf02bd861808c74
Dmitriy Boon <41760139+DmitriyBoon@users.noreply.github.com>
2018-10-29 18:27:23 +0300

    iptab fin

--- lesson.17.iptables/knock.sh
+++ lesson.17.iptables/knock.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# HOST=$1
-# shift
-# for ARG in "$@"
-# do
-#         sudo nmap -Pn --max-retries 0 -p $ARG $HOST
-# Done
-nmap -p 9911 192.168.255.1 &&  nmap -p 8811 192.168.255.1 && nmap -p 7711 192.168.255.1
+ HOST=$1
+ shift
+ for ARG in "$@"
+ do
+         sudo nmap -Pn --max-retries 0 -p $ARG $HOST
+ done
