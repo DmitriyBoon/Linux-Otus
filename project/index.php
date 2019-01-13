b5e33565bb6ddaa3a4300a7772959af1a5b088dd
Dmitriy <41760139+DmitriyBoon@users.noreply.github.com>
2019-01-13 23:57:10 +0300

    project

---
+++ project/index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "192.168.50.4";
+$username = "otus";
+$password = "1234";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
